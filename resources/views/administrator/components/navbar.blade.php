c3214ac5404e6f09d91862efab255a253f3b015e
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2023-09-02 09:23:03 +0700

    remove unuse

--- resources/views/administrator/components/navbar.blade.php
+++ resources/views/administrator/components/navbar.blade.php
@@ -13,78 +13,22 @@
                 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end" aria-labelledby="dropdownNotification">
                     <div>
                         <div class="border-bottom px-3 pt-2 pb-3 d-flex justify-content-between align-items-center">
-                            <p class="mb-0 text-dark fw-medium fs-4">Notifications</p>
-                            <a href="#" class="text-muted">
-                                <span>
-                                    <i class="me-1 icon-xxs" data-feather="settings"></i>
-                                </span>
-                            </a>
+                            <p class="mb-0 text-dark fw-medium fs-4">Notifikasi</p>
                         </div>
                         <!-- List group -->
                         <ul class="list-group list-group-flush notification-list-scroll">
-                            <!-- List group item -->
-                            <li class="list-group-item bg-light">
-
-
-                                <a href="#" class="text-muted">
-                                    <h5 class=" mb-1">Rishi Chopra</h5>
-                                    <p class="mb-0">
-                                        Mauris blandit erat id nunc blandit, ac eleifend dolor pretium.
-                                    </p>
-                                </a>
-
-
-
-                            </li>
-                            <!-- List group item -->
-                            <li class="list-group-item">
-
-
-                                <a href="#" class="text-muted">
-                                    <h5 class=" mb-1">Neha Kannned</h5>
-                                    <p class="mb-0">
-                                        Proin at elit vel est condimentum elementum id in ante. Maecenas et
-                                        sapien metus.
-                                    </p>
-                                </a>
-
-
-
-                            </li>
-                            <!-- List group item -->
-                            <li class="list-group-item">
-
-
-                                <a href="#" class="text-muted">
-                                    <h5 class=" mb-1">Nirmala Chauhan</h5>
-                                    <p class="mb-0">
-                                        Morbi maximus urna lobortis elit sollicitudin sollicitudieget elit
-                                        vel pretium.
-                                    </p>
-                                </a>
-
-
-
-                            </li>
-                            <!-- List group item -->
-                            <li class="list-group-item">
-
-
-                                <a href="#" class="text-muted">
-                                    <h5 class=" mb-1">Sina Ray</h5>
-                                    <p class="mb-0">
-                                        Sed aliquam augue sit amet mauris volutpat hendrerit sed nunc eu
-                                        diam.
-                                    </p>
-                                </a>
-
-
-
-                            </li>
+                            @foreach ([] as $item)
+                                <li class="list-group-item bg-light">
+                                    <a href="#" class="text-muted">
+                                        <h5 class=" mb-1">{{ $item->title }}</h5>
+                                        <p class="mb-0">{{ $item->description }}</p>
+                                    </a>
+                                </li>
+                            @endforeach
                         </ul>
                         <div class="border-top px-3 py-2 text-center">
                             <a href="#" class="text-inherit fw-semi-bold">
-                                View all Notifications
+                                Lihat semua
                             </a>
                         </div>
                     </div>
@@ -92,50 +36,28 @@
             </li>
             <!-- List -->
             <li class="dropdown ms-2">
-                <a class="rounded-circle" href="#" role="button" id="dropdownUser" data-bs-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
+                <a class="rounded-circle" href="#" role="button" id="dropdownUser" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="avatar avatar-md avatar-indicators avatar-online">
                         <img alt="avatar" src="{{ url('assets/images/avatar/avatar-1.jpg') }}" class="rounded-circle" />
                     </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownUser">
                     <div class="px-4 pb-0 pt-2">
-
-
                         <div class="lh-1 ">
-                            <h5 class="mb-1"> John E. Grainger</h5>
-                            <a href="#" class="text-inherit fs-6">View my profile</a>
+                            <h5 class="mb-1">{{ auth()->user()->name }}</h5>
+                            <a href="#" class="text-inherit fs-6">{{ auth()->user()->email }}</a>
                         </div>
                         <div class=" dropdown-divider mt-3 mb-2"></div>
                     </div>
-
                     <ul class="list-unstyled">
-
                         <li>
                             <a class="dropdown-item" href="#">
-                                <i class="me-2 icon-xxs dropdown-item-icon" data-feather="user"></i>Edit
-                                Profile
+                                <i class="me-2 icon-xxs dropdown-item-icon" data-feather="user"></i>Profil
                             </a>
                         </li>
                         <li>
-                            <a class="dropdown-item" href="#">
-                                <i class="me-2 icon-xxs dropdown-item-icon" data-feather="activity"></i>Activity Log
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item text-primary" href="#">
-                                <i class="me-2 icon-xxs text-primary dropdown-item-icon" data-feather="star"></i>Go Pro
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="#">
-                                <i class="me-2 icon-xxs dropdown-item-icon" data-feather="settings"></i>Account Settings
-                            </a>
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="./index.html">
-                                <i class="me-2 icon-xxs dropdown-item-icon" data-feather="power"></i>Sign
-                                Out
+                            <a class="dropdown-item" href="{{ route('logout') }}">
+                                <i class="me-2 icon-xxs dropdown-item-icon" data-feather="power"></i>Logout
                             </a>
                         </li>
                     </ul>
