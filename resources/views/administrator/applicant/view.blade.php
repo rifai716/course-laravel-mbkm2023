6c4ce62b6861baa61a2c993453fcbd18ecb246d0
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2023-09-03 22:53:23 +0700

    set view for pengembalian dan permintaan peminjaman

---
+++ resources/views/administrator/applicant/view.blade.php
@@ -0,0 +1,19 @@
+@extends('administrator.theme')
+@section('content')
+    <div class="container-fluid mt-n22 px-6">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-12">
+                <div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="mb-2 mb-lg-0 text-white">
+                            <h3 class="mb-0  text-white">
+                                <span class="mdi mdi-account-outline"></span>
+                                Data Permintaan Peminjaman
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
