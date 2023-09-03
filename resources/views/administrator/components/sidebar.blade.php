a59a9e19772bb259763696e4f9f9b407b6d69284
Ahmad Rifa'i <ahmadrifai716@gmail.com>
2023-09-03 23:43:21 +0700

    fix

--- resources/views/administrator/components/sidebar.blade.php
+++ resources/views/administrator/components/sidebar.blade.php
@@ -8,7 +8,7 @@
         <!-- Navbar nav -->
         <ul class="navbar-nav flex-column" id="sideNavbar">
             <li class="nav-item">
-                <a class="nav-link has-arrow  active " href="{{ route('administrator.dashboard') }}">
+                <a class="nav-link has-arrow {{ Request::segment(2) == 'dashboard' ? 'active' : '' }}" href="{{ route('administrator.dashboard') }}">
                     <i data-feather="home" class="nav-icon icon-xs me-2"></i> Dashboard
                 </a>
 
@@ -21,7 +21,7 @@
 
             <!-- Nav item -->
             <li class="nav-item">
-                <a class="nav-link has-arrow  collapsed " href="#!" data-bs-toggle="collapse"
+                <a class="nav-link has-arrow {{  in_array(Request::segment(2), ['admin', 'student']) ? 'active' : ' collapsed ' }}" href="#!" data-bs-toggle="collapse"
                     data-bs-target="#navPages" aria-expanded="false" aria-controls="navPages">
                     <i data-feather="users" class="nav-icon icon-xs me-2">
                     </i> Pengguna
@@ -30,12 +30,12 @@
                 <div id="navPages" class="collapse " data-bs-parent="#sideNavbar">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('administrator.admin') }}">
+                            <a class="nav-link {{ Request::segment(2) == 'admin' ? 'active' : '' }}" href="{{ route('administrator.admin') }}">
                                 Admin
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link has-arrow" href="{{ route('administrator.student') }}">
+                            <a class="nav-link {{ Request::segment(2) == 'student' ? 'active' : '' }}" href="{{ route('administrator.student') }}">
                                 Mahasiswa
                             </a>
                         </li>
@@ -44,21 +44,21 @@
 
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="{{ route('administrator.item.index') }}">
+                <a class="nav-link {{ Request::segment(2) == 'item' ? 'active' : '' }}" href="{{ route('administrator.item.index') }}">
                     <i data-feather="box" class="nav-icon icon-xs me-2">
                     </i>
                     Barang
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="{{ route('administrator.applicant') }}">
+                <a class="nav-link {{ Request::segment(2) == 'applicant-list' ? 'active' : '' }}" href="{{ route('administrator.applicant') }}">
                     <i data-feather="book-open" class="nav-icon icon-xs me-2">
                     </i>
                     Peminjaman
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="{{ route('administrator.return') }}">
+                <a class="nav-link {{ Request::segment(2) == 'return-list' ? 'active' : '' }}" href="{{ route('administrator.return') }}">
                     <i data-feather="book" class="nav-icon icon-xs me-2">
                     </i>
                     Pengembalian
